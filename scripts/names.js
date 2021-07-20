d937c327174fe579b042c57dd01df41ed1eeef00
jessicamargala <56134693+jessicamargala@users.noreply.github.com>
2021-07-20 14:39:58 -0700

    Re-Structures Files and adds Welcome page
    Adds four main folders: scripts, images, styles, & html. Adds a minimal welcome page that leads to simulation.

---
+++ scripts/names.js
@@ -0,0 +1,26 @@
+var dashboardTitle = "Platform Dashbaord";
+var controlsTitle = "Adminstrator Controls";
+var yAxisChart1 = "Rider Base Fees";
+var xAxisChart1 = "# of Potential Riders";
+var yAxisChart2 = "Driver Base Fees";
+var xAxisChart2 = "# of Potential Drivers";
+var subControls1 = "Pricing Policy";
+var subControls2 = "Membership Rules";
+var subControls3 = "Advertising";
+var riderFeesTitle = "Rider Fees";
+var driverFeesTitle = "Driver Fees";
+var surgePricingTitle = "Surge Pricing";
+var crimePricingTitle = "Crime Pricing";
+var riderStarsTitle = "Rider Stars";
+var driverStarsTitle = "Driver Stars";
+var lowBatteryTitle = "Low Battery";
+var banRidersTitle = "Ban Riders";
+var banDriversTitle = "Ban Drivers";
+var riderRedressTitle = "Rider Redress";
+var driverRedressTitle = "Driver Redress";
+var driverAdsTitle = "Driver-Directed Ads";
+var riderAdsTitle = "Rider-Directed Ads";
+
+function docWrite(variable) {
+  document.write(variable);
+}
